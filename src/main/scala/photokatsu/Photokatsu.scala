0b11d819f8fb411bd83bcba359d28c36c408c963
Walfie <walfington@gmail.com>
2016-02-01 23:55:42 -0500

    Testing some runtime stuff

--- src/main/scala/photokatsu/Photokatsu.scala
+++ src/main/scala/photokatsu/Photokatsu.scala
@@ -4,18 +4,35 @@ import photokatsu.Idols._
 
 object Photokatsu {
   def main(args: Array[String]): Unit = {
-    val myIdols: Seq[Idol] = Seq(
+    /*
+    val myIdols: Set[Idol] = Set(
       Akari, Ichigo, Maria, Yurika, Sumire, Rin,
-      Seira, Shion, Sora,Ran, Kii, Mikuru, Mizuki, Kokone
+      Seira, Shion, Sora, Ran, Kii,
+      Mikuru, Mizuki, Kokone
+    )
+    */
+
+    val myIdols: Seq[Idol] = scala.util.Random.shuffle(
+      Seq(
+        Akari, Aoi, Hikari, Hinaki, Ichigo,
+        Juri, Kaede, Kii, Kokone, Madoka,
+        Maria, Mikuru, Miyabi, Mizuki, Otome,
+        Ran, Rin, Sakura, Seira, Shion,
+        Sora, Sumire, Yurika
+      )
+    ).take(20)
+
+    val prioritizedIdols: Set[Idol] = Set(
+      Akari, Yurika, Sumire
     )
 
-    val subunits = getPossibleSubunits(myIdols, Subunits.all)
+    val subunits = getPossibleSubunits(myIdols.toSeq, Subunits.all)
 
-    val units: Array[IdolUnit] = IdolUnits.fromSubunits(subunits, 1)
+    val units: Array[IdolUnit] = IdolUnits.fromSubunits(subunits, 7)
+      .sortBy(unit => -unit.subunits.size)
+      .sortBy(unit => -prioritizedIdols.intersect(unit.members).size)
 
-    for {
-      unit <- units.toArray.sortBy(-_.subunits.size)
-    } {
+    for (unit <- units) {
       println(unit.subunits.size)
       println(unit.members.mkString(", "))
       println(unit.subunits.map(_.name).mkString("(", ", ", ")"))
